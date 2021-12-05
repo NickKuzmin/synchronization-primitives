87293d3757b2aa2fe2a9acfe75ab7c963c937dc6
Nikita Kuzmin <kuzmin.nick@rambler.ru>
2021-12-05 13:48:06 +0300

    Initial commit

---
+++ SynchronizationPrimitives.DotNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("SynchronizationPrimitives.DotNet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SynchronizationPrimitives.DotNet")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8124c4cc-3731-4347-8575-253017191526")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SynchronizationPrimitives.Mutex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SynchronizationPrimitives.Mutex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mutex = new System.Threading.Mutex(false, @"Global\" + Guid.Empty);
+            GC.KeepAlive(mutex);
+
+            if (!mutex.WaitOne(0, false))
+            {
+                Console.WriteLine("Instance already running");
+            }
+            else
+            {
+                Console.WriteLine("First load!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
