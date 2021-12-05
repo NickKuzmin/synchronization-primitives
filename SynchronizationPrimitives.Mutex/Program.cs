de5aba73753574c27715c0d5ad860cadb69e2031
Nikita Kuzmin <kuzmin.nick@rambler.ru>
2021-12-05 13:50:01 +0300

    Mutex

--- SynchronizationPrimitives.Mutex/Program.cs
+++ SynchronizationPrimitives.Mutex/Program.cs
@@ -7,8 +7,6 @@ namespace SynchronizationPrimitives.Mutex
         static void Main(string[] args)
         {
             var mutex = new System.Threading.Mutex(false, @"Global\" + Guid.Empty);
-            GC.KeepAlive(mutex);
-
             if (!mutex.WaitOne(0, false))
             {
                 Console.WriteLine("Instance already running");
