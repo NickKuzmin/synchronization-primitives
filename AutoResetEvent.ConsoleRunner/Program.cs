6f655579a0b108a0314e633e6f9226c18660abed
Nikita Kuzmin <kuzmin.nick@rambler.ru>
2021-12-05 15:23:24 +0300

    AutoResetEvent

---
+++ AutoResetEvent.ConsoleRunner/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoResetEvent.ConsoleRunner
+{
+    public class Program
+    {
+        private static readonly System.Threading.AutoResetEvent WaitHandler = new System.Threading.AutoResetEvent(true);
+        private static int _counter = 0;
+
+        public static void Main(string[] args)
+        {
+            for (var i = 1; i < 6; i++)
+            {
+                var myThread = new Thread(Count)
+                {
+                    Name = $"Thread {i}"
+                };
+                myThread.Start();
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void Count()
+        {
+            WaitHandler.WaitOne();
+            _counter = 1;
+            for (var i = 1; i < 6; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: {_counter}");
+                _counter++;
+                Thread.Sleep(100);
+            }
+
+            WaitHandler.Set();
+        }
+    }
+}
---
+++ Locker.ConsoleRunner/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace Locker.ConsoleRunner
+{
+    public class Program
+    {
+        private static readonly object Locker = new object();
+        private static int _counter = 0;
+
+        public static void Main(string[] args)
+        {
+            for (var i = 1; i < 6; i++)
+            {
+                var myThread = new Thread(Count)
+                {
+                    Name = $"Thread {i}"
+                };
+                myThread.Start();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Count()
+        {
+            lock (Locker)
+            {
+                _counter = 1;
+                for (int i = 1; i < 6; i++)
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.Name}: {_counter}");
+                    _counter++;
+                    Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
